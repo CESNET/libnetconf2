5da708a2926fade42d19fd555a00b4979500ecae
Radek Krejci <rkrejci@cesnet.cz>
2015-09-01 17:33:23 +0200

    FEATURE add main header files
    Split public API into 3 parts
    - nc_client.h - API for NETCONF clients
    - nc_server.h - API for NETCONF servers
    - nc_transapi.h - API for libnetconf's transAPI modules

---
+++ nc_server.h
@@ -0,0 +1,28 @@
+/**
+ * \file nc_server.h
+ * \author Radek Krejci <rkrejci@cesnet.cz>
+ * \brief libnetconf2's main public header for NETCONF servers.
+ *
+ * Copyright (c) 2015 CESNET, z.s.p.o.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name of the Company nor the names of its contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ */
+
+#ifndef NC_SERVER_H_
+#define NC_SERVER_H_
+
+#include <libnetconf2/log.h>
+
+#endif /* NC_SERVER_H_ */
