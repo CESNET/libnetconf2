4fefae32c09ef17f6dce6292fa4d540cab3f7115
Michal Vasko <mvasko@cesnet.cz>
2016-07-21 13:52:24 +0200

    travis MAINTENANCE removed redundant commands

--- .travis-deps-osx.sh
+++ .travis-deps-osx.sh
@@ -1,39 +1,27 @@
+#!/bin/sh
+set -e
+
 #install dependencies using homebrew
 brew update
 brew upgrade openssl
 brew link --force openssl
 brew install pcre
 
-set -e
+# libssh
+wget https://git.libssh.org/projects/libssh.git/snapshot/libssh-0.7.3.tar.bz2
+tar -xjf libssh-0.7.3.tar.bz2
+mkdir libssh-0.7.3/build && cd libssh-0.7.3/build
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr .. && make -j2 && sudo make install
+cd ../..
 
-INSTALL_PREFIX_DIR=$HOME/local
-export PKG_CONFIG_PATH=$INSTALL_PREFIX_DIR/lib/pkgconfig:$PKG_CONFIG_PATH
-
-# use cache if present
-if [ ! -d "$INSTALL_PREFIX_DIR/lib" ]; then
-  echo "Building cache."
-  cd ~
-
-  # libssh
-  wget https://git.libssh.org/projects/libssh.git/snapshot/libssh-0.7.3.tar.bz2
-  tar -xjf libssh-0.7.3.tar.bz2
-  mkdir libssh-0.7.3/build && cd libssh-0.7.3/build
-  cmake -DCMAKE_SHARED_LINKER_FLAGS=-L/usr/local/opt/openssl/lib -DCMAKE_C_FLAGS=-I/usr/local/opt/openssl/include -DCMAKE_INSTALL_PREFIX:PATH=/usr .. && make -j2 && sudo make install
-  cd ../..
-
-  # CMocka
-  wget https://cmocka.org/files/1.0/cmocka-1.0.1.tar.xz
-  tar -xJf cmocka-1.0.1.tar.xz
-  mkdir cmocka-1.0.1/build && cd cmocka-1.0.1/build
-  cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr .. && make -j2 && sudo make install
-  cd ../..
-
-else
-  echo "Using cache ($INSTALL_PREFIX_DIR)."
-fi
+# CMocka
+wget https://cmocka.org/files/1.0/cmocka-1.0.1.tar.xz
+tar -xJf cmocka-1.0.1.tar.xz
+mkdir cmocka-1.0.1/build && cd cmocka-1.0.1/build
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr .. && make -j2 && sudo make install
+cd ../..
 
 git clone -b $TRAVIS_BRANCH https://github.com/CESNET/libyang.git
 mkdir libyang/build && cd libyang/build
 cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr .. && make -j2 && sudo make install
 cd ../..
-
