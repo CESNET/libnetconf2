cb1acf21445f2238f7b24a1092fcbe4883cc448b
Radek Krejci <rkrejci@cesnet.cz>
2017-05-26 13:53:32 +0200

    build CHANGE update libssh used for TravisCI build

--- .travis-deps-linux.sh
+++ .travis-deps-linux.sh
@@ -10,9 +10,9 @@ sudo apt-get install -y valgrind
 echo -n | openssl s_client -connect scan.coverity.com:443 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | sudo tee -a /etc/ssl/certs/ca-certificates.crt
 
 # libssh
-wget https://red.libssh.org/attachments/download/195/libssh-0.7.3.tar.xz
-tar -xJf libssh-0.7.3.tar.xz
-mkdir libssh-0.7.3/build && cd libssh-0.7.3/build
+wget https://git.libssh.org/projects/libssh.git/snapshot/libssh-0.7.5.tar.gz
+tar -xzf libssh-0.7.5.tar.gz
+mkdir libssh-0.7.5/build && cd libssh-0.7.5/build
 cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr .. && make -j2 && sudo make install
 cd ../..
 
