1db8fdce4408e9db09bbc5771c87a496b07d7ca7
David Sedlák <xsedla1d@stud.fit.vutbr.cz>
2018-08-31 12:48:57 +0200

    tests ADD new test cases to test_client_ssh

--- tests/client/test_client_ssh.c
+++ tests/client/test_client_ssh.c
@@ -14,11 +14,32 @@
 #include <libssh/callbacks.h>
 #include <libssh/server.h>
 
+#define VALID_HELLO "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"                \
+                    "<hello xmlns=\"urn:ietf:params:xml:ns:netconf:base:1.0\">" \
+                    "<capabilities>"                                            \
+                    "<capability>"                                              \
+                    "urn:ietf:params:netconf:base:1.1"                          \
+                    "</capability>"                                             \
+                    "<capability>"                                              \
+                    "urn:ietf:params:ns:netconf:capability:startup:1.0"         \
+                    "</capability>"                                             \
+                    "</capabilities>"                                           \
+                    "<session-id>4</session-id>"                                \
+                    "</hello>"                                                  \
+                    "]]>]]>";
+
+#define VALID_OK    "\n#126\n"                                                  \
+                    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"              \
+                    "<rpc-reply id=\"106\"\n"                                   \
+                    "xmlns=\"urn:ietf:params:xml:ns:netconf:base:1.0\">\n"      \
+                    "<ok/>\n"                                                   \
+                    "</rpc-reply>"                                              \
+                    "\n##\n";
+
 /* variable to store pointer to msg used by __wrap_ssh_channel_read */
 const char *msg = NULL;
 /* variable to count how many bytes of msg are already read in __wrap_ssh_channel_read */
 uint32_t already_read = 0;
-
 
 static int
 ssh_hostkey_check_clb(const char *hostname, ssh_session session, void *priv)
@@ -34,10 +55,11 @@ static int
 setup_f(void **state)
 {
     (void)state;
-    //nc_verbosity(NC_VERB_VERBOSE);
     already_read = 0;
     msg = NULL;
     int ret;
+
+    nc_verbosity(NC_VERB_VERBOSE);
 
     /* initiate client */
     nc_client_init();
@@ -71,6 +93,7 @@ __wrap_ssh_connect(ssh_session session)
 {
     (void)session;
 
+    /* set support of all authentication methods by fake server */
     ssh_set_auth_methods(session, SSH_AUTH_METHOD_PUBLICKEY | SSH_AUTH_METHOD_PASSWORD | SSH_AUTH_METHOD_INTERACTIVE);
     return (int)mock();
 }
@@ -80,6 +103,7 @@ __wrap_ssh_userauth_none(ssh_session session, const char *username)
 {
     (void)session;
     (void)username;
+
     return (int)mock();
 }
 
@@ -151,12 +175,25 @@ __wrap_ssh_channel_read(ssh_channel channel, void *dest, uint32_t count, int is_
     (void)channel;
     (void)is_stderr;
 
+    /* read message stored in global variable msg */
+    /* global variable already_read is counter of how many bytes of msg are read */
     uint32_t msg_len = strlen(msg);
     uint32_t to_read_len = (msg_len > (already_read + count)) ? count : msg_len - already_read;
     memcpy(dest, &msg[already_read], to_read_len);
     already_read += to_read_len;
 
     return to_read_len;
+}
+
+int
+__wrap_ssh_userauth_password(ssh_session session, const char *username, const char *password)
+{
+    (void)session;
+    check_expected(password);
+    check_expected(username);
+
+
+    return (int)mock();
 }
 
 static int
@@ -170,15 +207,18 @@ test_hostkey_clb(const char *hostname, ssh_session session, void *priv)
 }
 
 static void
-test_nc_client_ssh_seting_auth_hostkey_check_clb(void **state)
+test_nc_client_ssh_setting_auth_hostkey_check_clb(void **state)
 {
     (void)state;
     int (*ret_f)(const char *hostname, ssh_session session, void *priv);
     char *priv_data_ret;
+
+    /* ssh_hostkey_check_clb is set in setup_f */
     nc_client_ssh_get_auth_hostkey_check_clb(&ret_f, (void **)&priv_data_ret);
     assert_ptr_equal(ret_f, ssh_hostkey_check_clb);
     assert_null(priv_data_ret);
 
+    /* set different callback and private data */
     nc_client_ssh_set_auth_hostkey_check_clb(test_hostkey_clb, "DATA");
     nc_client_ssh_get_auth_hostkey_check_clb(&ret_f, (void **)&priv_data_ret);
     assert_ptr_equal(ret_f, test_hostkey_clb);
@@ -188,11 +228,17 @@ test_nc_client_ssh_seting_auth_hostkey_check_clb(void **state)
 char *
 test_pwd_clb1(const char *username, const char *hostname, void *priv)
 {
-    (void)username;
-    (void)hostname;
-    (void)priv;
+    char *pass, *pass_to_return;
 
-    return 0;
+    check_expected(username);
+    check_expected(hostname);
+    check_expected(priv);
+
+    pass = (char *)mock();
+    pass_to_return = malloc(sizeof *pass * (strlen(pass) + 1));
+    strcpy(pass_to_return, pass);
+
+    return pass_to_return;
 }
 
 char *
@@ -212,11 +258,13 @@ test_nc_client_ssh_setting_auth_password_clb(void **state)
     char *(*ret_f)(const char *username, const char *hostname, void *priv);
     char *priv_data_ret;
 
+    /* set callback */
     nc_client_ssh_set_auth_password_clb(test_pwd_clb1, "DATA");
     nc_client_ssh_get_auth_password_clb(&ret_f, (void **)&priv_data_ret);
     assert_ptr_equal(test_pwd_clb1, ret_f);
     assert_string_equal("DATA", priv_data_ret);
 
+    /* set different callback */
     nc_client_ssh_set_auth_password_clb(test_pwd_clb2, "NEW DATA");
     nc_client_ssh_get_auth_password_clb(&ret_f, (void **)&priv_data_ret);
     assert_ptr_equal(test_pwd_clb2, ret_f);
@@ -253,11 +301,14 @@ test_nc_client_ssh_setting_auth_interactive_clb(void **state)
     (void)state;
     char *(*ret_f)(const char *auth_name, const char *instruction, const char *prompt, int echo, void *priv);
     char *priv_data_ret;
+
+    /* set callback */
     nc_client_ssh_set_auth_interactive_clb(test_inter_clb1, "DATA");
     nc_client_ssh_get_auth_interactive_clb(&ret_f, (void **)&priv_data_ret);
     assert_ptr_equal(test_inter_clb1, ret_f);
     assert_string_equal("DATA", priv_data_ret);
 
+    /* set diferent callback */
     nc_client_ssh_set_auth_interactive_clb(test_inter_clb2, "NEW DATA");
     nc_client_ssh_get_auth_interactive_clb(&ret_f, (void **)&priv_data_ret);
     assert_ptr_equal(test_inter_clb2, ret_f);
@@ -288,11 +339,13 @@ test_nc_client_ssh_setting_auth_privkey_passphrase_clb(void **state)
     (void)state;
     char *(*ret_f)(const char *privkey_path, void *priv);
     char *priv_data_ret;
+    /* set first callback */
     nc_client_ssh_set_auth_privkey_passphrase_clb(test_passphrase_clb1, "DATA");
     nc_client_ssh_get_auth_privkey_passphrase_clb(&ret_f, (void **)&priv_data_ret);
     assert_ptr_equal(ret_f, test_passphrase_clb1);
     assert_string_equal("DATA", priv_data_ret);
 
+    /* set different callback */
     nc_client_ssh_set_auth_privkey_passphrase_clb(test_passphrase_clb2, "NEW DATA");
     nc_client_ssh_get_auth_privkey_passphrase_clb(&ret_f, (void **)&priv_data_ret);
     assert_ptr_equal(ret_f, test_passphrase_clb2);
@@ -304,7 +357,7 @@ test_nc_client_ssh_adding_keypair(void **state)
 {
     (void)state;
     int ret;
-    /* TODO test also nc_client_ssh_get_keypair */
+    const char *pubkey1, *pubkey2;
     /* at the beginning keypair count should be 0 */
     ret = nc_client_ssh_get_keypair_count();
     assert_int_equal(ret, 0);
@@ -320,12 +373,19 @@ test_nc_client_ssh_adding_keypair(void **state)
     assert_int_equal(ret, 0);
     ret = nc_client_ssh_get_keypair_count();
     assert_int_equal(ret, 2);
+    ret = nc_client_ssh_get_keypair(1, &pubkey1, &pubkey2);
+    assert_int_equal(ret, 0);
+    assert_string_equal(pubkey1, "key_pub");
+    assert_string_equal(pubkey2, "key_priv");
 
     /* delete first keypair */
     ret = nc_client_ssh_del_keypair(0);
     assert_int_equal(ret, 0);
     ret = nc_client_ssh_get_keypair_count();
     assert_int_equal(ret, 1);
+    /* try to get deleted keypair */
+    ret = nc_client_ssh_get_keypair(5, &pubkey1, &pubkey2);
+    assert_int_equal(ret, -1);
 
     /* try to add keypair that is already set */
     ret = nc_client_ssh_add_keypair("key_pub", "key_priv");
@@ -339,7 +399,7 @@ test_nc_client_ssh_adding_keypair(void **state)
     ret = nc_client_ssh_get_keypair_count();
     assert_int_equal(ret, 1);
 
-    /* remove keypairs */
+    /* remove remaining keypairs */
     ret = nc_client_ssh_del_keypair(0);
     assert_int_equal(ret, 0);
     ret = nc_client_ssh_get_keypair_count();
@@ -347,26 +407,74 @@ test_nc_client_ssh_adding_keypair(void **state)
 }
 
 static void
-test_nc_connect_ssh_succesfull(void **state)
+test_nc_client_ssh_setting_auth_pref(void **state)
+{
+    (void)state;
+    int ret;
+    /* check default prefference settings according to documentation */
+    ret = nc_client_ssh_get_auth_pref(NC_SSH_AUTH_INTERACTIVE);
+    assert_int_equal(ret, 3);
+    ret = nc_client_ssh_get_auth_pref(NC_SSH_AUTH_PASSWORD);
+    assert_int_equal(ret, 2);
+    ret = nc_client_ssh_get_auth_pref(NC_SSH_AUTH_PUBLICKEY);
+    assert_int_equal(ret, 1);
+
+    /* try to set prefetence of non existing method */
+    nc_client_ssh_set_auth_pref(42, 22);
+
+    /* try to get preference of non existing method */
+    ret = nc_client_ssh_get_auth_pref(42);
+    assert_int_equal(ret, 0);
+
+    /* change values of all methods and check if they actually changed */
+    nc_client_ssh_set_auth_pref(NC_SSH_AUTH_INTERACTIVE, 9);
+    ret = nc_client_ssh_get_auth_pref(NC_SSH_AUTH_INTERACTIVE);
+    assert_int_equal(ret, 9);
+
+    /* negative value should be set as -1 */
+    nc_client_ssh_set_auth_pref(NC_SSH_AUTH_PASSWORD, -5);
+    ret = nc_client_ssh_get_auth_pref(NC_SSH_AUTH_PASSWORD);
+    assert_int_equal(ret, -1);
+
+    nc_client_ssh_set_auth_pref(NC_SSH_AUTH_PUBLICKEY, 11);
+    ret = nc_client_ssh_get_auth_pref(NC_SSH_AUTH_PUBLICKEY);
+    assert_int_equal(ret, 11);
+}
+
+static void
+test_nc_client_ssh_setting_username(void **state)
+{
+    (void)state;
+    int ret;
+    const char *username_ret;
+    username_ret = nc_client_ssh_get_username();
+    /* username is set to "username" in setup_f */
+    assert_string_equal(username_ret, "username");
+
+    /* set new username and check if it changes */
+    ret = nc_client_ssh_set_username("new_username");
+    assert_int_equal(ret, 0);
+    username_ret = nc_client_ssh_get_username();
+    assert_string_equal(username_ret, "new_username");
+}
+
+static void
+test_nc_connect_ssh_interactive_succesfull(void **state)
 {
     (void)state;
     struct nc_session *session;
 
     /* set valid hello message */
-    msg = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-          "<hello xmlns=\"urn:ietf:params:xml:ns:netconf:base:1.0\">"
-          "<capabilities>"
-          "<capability>"
-          "urn:ietf:params:netconf:base:1.1"
-          "</capability>"
-          "<capability>"
-          "urn:ietf:params:ns:netconf:capability:startup:1.0"
-          "</capability>"
-          "</capabilities>"
-          "<session-id>4</session-id>"
-          "</hello>"
-          "]]>]]>";
+    msg = VALID_HELLO;
 
+    /* set authentication method to use interactive authentication */
+    nc_client_ssh_set_auth_pref(NC_SSH_AUTH_INTERACTIVE, 1);
+    nc_client_ssh_set_auth_pref(NC_SSH_AUTH_PASSWORD, -1);
+    nc_client_ssh_set_auth_pref(NC_SSH_AUTH_PUBLICKEY, -1);
+
+    nc_client_ssh_set_auth_pref(NC_SSH_AUTH_INTERACTIVE, 20);
+
+    /* prepare return values for functions used by nc_connect_ssh */
     will_return(__wrap_connect, 0);
     will_return(__wrap_ssh_connect, 0);
     will_return(__wrap_ssh_userauth_none, 1);
@@ -387,14 +495,9 @@ test_nc_connect_ssh_succesfull(void **state)
 
     already_read = 0;
     /* set valid ok reply */
-    msg = "\n#126\n"
-          "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-          "<rpc-reply id=\"106\"\n"
-          "xmlns=\"urn:ietf:params:xml:ns:netconf:base:1.0\">\n"
-          "<ok/>\n"
-          "</rpc-reply>"
-          "\n##\n";
+    msg = VALID_OK;
 
+    /* prepare return values used by nc_session_free */
     will_return(__wrap_ssh_is_connected, 1);
     will_return(__wrap_ssh_is_connected, 1);
     will_return(__wrap_ssh_is_connected, 1);
@@ -405,24 +508,83 @@ test_nc_connect_ssh_succesfull(void **state)
 }
 
 static void
+test_nc_connect_ssh_password_succesfull(void **state)
+{
+    (void)state;
+    struct nc_session *session;
+
+    /* set valid hello message */
+    msg = VALID_HELLO;
+
+    /* set authentication method to use password authentication */
+    nc_client_ssh_set_auth_pref(NC_SSH_AUTH_PASSWORD, 1);
+    nc_client_ssh_set_auth_pref(NC_SSH_AUTH_PUBLICKEY, -1);
+    nc_client_ssh_set_auth_pref(NC_SSH_AUTH_INTERACTIVE, -1);
+
+    /* set authentication callback */
+    nc_client_ssh_set_auth_password_clb(test_pwd_clb1, "private_data");
+    will_return(test_pwd_clb1, "secret password");
+    /* set values that are expected as parameters for authentication callback */
+    expect_string(test_pwd_clb1, username, "username");
+    expect_string(test_pwd_clb1, hostname, "127.0.0.1");
+    expect_string(test_pwd_clb1, priv, "private_data");
+
+    /* fake succesfull connection */
+    will_return(__wrap_connect, 0);
+    will_return(__wrap_ssh_connect, 0);
+    /* do not authenticate using no authentication method */
+    will_return(__wrap_ssh_userauth_none, 1);
+
+    /* succesfully authenticate via password authentication */
+    expect_string(__wrap_ssh_userauth_password, password, "secret password");
+    expect_string(__wrap_ssh_userauth_password, username, "username");
+    will_return(__wrap_ssh_userauth_password, 0);
+
+    /* fake that connection is still alive */
+    will_return(__wrap_ssh_is_connected, 1);
+    will_return(__wrap_ssh_is_connected, 1);
+
+    /* fake ssh functions that are used to open netconf channel */
+    will_return(__wrap_ssh_channel_open_session, 0);
+    will_return(__wrap_ssh_channel_request_subsystem, 0);
+
+    /* fake that connection is still alive*/
+    will_return(__wrap_ssh_is_connected, 1);
+
+    /* fake ssh function for recieving hello message */
+    will_return(__wrap_ssh_channel_poll_timeout, 1);
+    will_return(__wrap_ssh_is_connected, 1);
+
+    session = nc_connect_ssh("127.0.0.1", 8080, NULL);
+    assert_non_null(session);
+
+    /* prepare to read new message */
+    already_read = 0;
+    /* set valid ok reply */
+    msg = VALID_OK;
+
+    /* prepare return values used by nc_session_free */
+    will_return(__wrap_ssh_is_connected, 1);
+    will_return(__wrap_ssh_is_connected, 1);
+    will_return(__wrap_ssh_is_connected, 1);
+    will_return(__wrap_ssh_channel_poll_timeout, 1);
+
+    /* disconnect */
+    nc_session_free(session, NULL);
+}
+
+static void
 test_nc_connect_ssh_malformed_hello(void **state)
 {
     (void)state;
     struct nc_session *session;
 
     /* set malformed hello message */
-    msg = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-          "<hello xmlns=\"urn:ietf:params:xml:ns:netconf:base:1.0\">"
-          "<capabilities>"
-          "<capability>"
-          "urn:ietf:params:netconf:base:1.1"
+    msg = "<?xml ve\"1.0\" ding-8\"?>"
+          "<hello xmlns=\"urn:ietf:pml:ns:netconf:base:1.0\">"
+          "<capiapability:startup:1.0"
           "</capability>"
-          "<capability>"
-          "urn:iet:capability:startup:1.0"
-          "</capability>"
-          "</capes>"
           "<session-idssion-id>"
-          "</>"
           "]]>]]>";
 
     will_return(__wrap_connect, 0);
@@ -485,15 +647,18 @@ int
 main(void)
 {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test_setup_teardown(test_nc_client_ssh_seting_auth_hostkey_check_clb, setup_f, teardown_f),
+        cmocka_unit_test_setup_teardown(test_nc_client_ssh_setting_auth_hostkey_check_clb, setup_f, teardown_f),
         cmocka_unit_test_setup_teardown(test_nc_client_ssh_setting_auth_password_clb, setup_f, teardown_f),
         cmocka_unit_test_setup_teardown(test_nc_client_ssh_setting_auth_interactive_clb, setup_f, teardown_f),
         cmocka_unit_test_setup_teardown(test_nc_client_ssh_setting_auth_privkey_passphrase_clb, setup_f, teardown_f),
         cmocka_unit_test_setup_teardown(test_nc_client_ssh_adding_keypair, setup_f, teardown_f),
-        cmocka_unit_test_setup_teardown(test_nc_connect_ssh_succesfull, setup_f, teardown_f),
+        cmocka_unit_test_setup_teardown(test_nc_client_ssh_setting_auth_pref, setup_f, teardown_f),
+        cmocka_unit_test_setup_teardown(test_nc_client_ssh_setting_username, setup_f, teardown_f),
         cmocka_unit_test_setup_teardown(test_nc_connect_connection_failed, setup_f, teardown_f),
         cmocka_unit_test_setup_teardown(test_nc_connect_ssh_malformed_hello, setup_f, teardown_f),
         cmocka_unit_test_setup_teardown(test_nc_connect_ssh_disconnected, setup_f, teardown_f),
+        cmocka_unit_test_setup_teardown(test_nc_connect_ssh_interactive_succesfull, setup_f, teardown_f),
+        cmocka_unit_test_setup_teardown(test_nc_connect_ssh_password_succesfull, setup_f, teardown_f),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
