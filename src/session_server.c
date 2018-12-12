CESNET/libnetconf2

commit ac7fa2fb48a3a01200416374cf555f39625e3826

add support for unix socket transport
In addition to SSH and TLS transport, add the support for UNIX
socket. Despite it's not in NETCONF standard, using a UNIX socket has
several advantages:

- no need of password, keys, or certificate. The connection is allowed
  for a local user authenticated on the system, and having the proper
  permissions to connect to the netopeer socket.

- the connection to netopeer is possible even if the firewall is
  enabled. This is an advantage when configuring a firewall equipment
  through netconf.

- the connection to netopeer is possible from another netns.

Signed-off-by: Olivier Matz <olivier.matz@6wind.com>

src/session_server.c
@@ -11,7 +11,7 @@
  *
  *     https://opensource.org/licenses/BSD-3-Clause
  */
-#define _GNU_SOURCE /* signals, threads */
+#define _GNU_SOURCE /* signals, threads, SO_PEERCRED */
 
 #include <stdint.h>
 #include <stdlib.h>
@@ -20,12 +20,16 @@
 #include <poll.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/un.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <pthread.h>
 #include <time.h>
 #include <signal.h>
+#include <pwd.h>
 
 #include "libnetconf.h"
 #include "session_server.h"
@@ -160,7 +164,7 @@ nc_session_set_status(struct nc_session *session, NC_STATUS status)
 }
 
 int
-nc_sock_listen(const char *address, uint16_t port)
+nc_sock_listen_inet(const char *address, uint16_t port)
 {
     int opt;
     int is_ipv4, sock;
@@ -229,6 +233,57 @@ nc_sock_listen(const char *address, uint16_t port)
 
     if (listen(sock, NC_REVERSE_QUEUE) == -1) {
         ERR("Unable to start listening on \"%s\" port %d (%s).", address, port, strerror(errno));
+        goto fail;
+    }
+
+    return sock;
+
+fail:
+    if (sock > -1) {
+        close(sock);
+    }
+
+    return -1;
+}
+
+int
+nc_sock_listen_unix(const char *address, const struct nc_server_unix_opts *opts)
+{
+    struct sockaddr_un sun;
+    int sock = -1;
+
+    sock = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sock == -1) {
+        ERR("Failed to create socket (%s).", strerror(errno));
+        goto fail;
+    }
+
+    memset(&sun, 0, sizeof(sun));
+    sun.sun_family = AF_UNIX;
+    snprintf(sun.sun_path, sizeof(sun.sun_path), "%s", address);
+
+    unlink(sun.sun_path);
+    if (bind(sock, (struct sockaddr *)&sun, sizeof(sun)) == -1) {
+        ERR("Could not bind \"%s\" (%s).", address, strerror(errno));
+        goto fail;
+    }
+
+    if (opts->mode != (mode_t)-1) {
+        if (chmod(sun.sun_path, opts->mode) < 0) {
+            ERR("Failed to set unix socket permissions (%s).", strerror(errno));
+            goto fail;
+        }
+    }
+
+    if (opts->uid != (uid_t)-1 || opts->gid != (gid_t)-1) {
+        if (chown(sun.sun_path, opts->uid, opts->gid) < 0) {
+            ERR("Failed to set unix socket uid/gid (%s).", strerror(errno));
+            goto fail;
+        }
+    }
+
+    if (listen(sock, NC_REVERSE_QUEUE) == -1) {
+        ERR("Unable to start listening on \"%s\" (%s).", address, strerror(errno));
         goto fail;
     }
 
@@ -334,6 +389,34 @@ nc_sock_accept_binds(struct nc_bind *binds, uint16_t bind_count, int timeout, ch
         return -1;
     }
 
+    if (saddr.ss_family == AF_INET || saddr.ss_family == AF_INET6) {
+        /* enable keep-alive */
+#ifdef TCP_KEEPIDLE
+        flags = 1;
+        if (setsockopt(ret, IPPROTO_TCP, TCP_KEEPIDLE, &flags, sizeof flags) == -1) {
+            ERR("Setsockopt failed (%s).", strerror(errno));
+            close(ret);
+            return -1;
+        }
+#endif
+#ifdef TCP_KEEPINTVL
+        flags = 5;
+        if (setsockopt(ret, IPPROTO_TCP, TCP_KEEPINTVL, &flags, sizeof flags) == -1) {
+            ERR("Setsockopt failed (%s).", strerror(errno));
+            close(ret);
+            return -1;
+        }
+#endif
+#ifdef TCP_KEEPCNT
+        flags = 10;
+        if (setsockopt(ret, IPPROTO_TCP, TCP_KEEPCNT, &flags, sizeof flags) == -1) {
+            ERR("Setsockopt failed (%s).", strerror(errno));
+            close(ret);
+            return -1;
+        }
+#endif
+    }
+
     if (idx) {
         *idx = i;
     }
@@ -366,6 +449,15 @@ nc_sock_accept_binds(struct nc_bind *binds, uint16_t bind_count, int timeout, ch
 
                 if (port) {
                     *port = ntohs(((struct sockaddr_in6 *)&saddr)->sin6_port);
+                }
+            } else {
+                ERRMEM;
+            }
+        } else if (saddr.ss_family == AF_UNIX) {
+            *host = strdup(((struct sockaddr_un *)&saddr)->sun_path);
+            if (*host) {
+                if (port) {
+                    *port = 0;
                 }
             } else {
                 ERRMEM;
@@ -691,6 +783,45 @@ nc_accept_inout(int fdin, int fdout, const char *username, struct nc_session **s
     (*session)->status = NC_STATUS_RUNNING;
 
     return msgtype;
+}
+
+static int
+nc_accept_unix(struct nc_session *session, int sock)
+{
+    const struct passwd *pw;
+    struct ucred ucred;
+    char *username;
+    socklen_t len;
+
+    session->ti_type = NC_TI_UNIX;
+
+    len = sizeof(ucred);
+    if (getsockopt(sock, SOL_SOCKET, SO_PEERCRED, &ucred, &len) < 0) {
+        ERR("Failed to get credentials from unix socket (%s).",
+            strerror(errno));
+        close(sock);
+        return -1;
+    }
+
+    pw = getpwuid(ucred.uid);
+    if (pw == NULL) {
+        ERR("Failed to find username for uid=%u (%s).\n", ucred.uid,
+            strerror(errno));
+        close(sock);
+        return -1;
+    }
+
+    username = strdup(pw->pw_name);
+    if (username == NULL) {
+        ERRMEM;
+        close(sock);
+        return -1;
+    }
+    session->username = lydict_insert_zc(server_opts.ctx, username);
+
+    session->ti.unixsock.sock = sock;
+
+    return 1;
 }
 
 static void
@@ -1316,7 +1447,8 @@ nc_ps_poll_session_io(struct nc_session *session, int io_timeout, time_t now_mon
         break;
 #endif
     case NC_TI_FD:
-        pfd.fd = session->ti.fd.in;
+    case NC_TI_UNIX:
+        pfd.fd = (session->ti_type == NC_TI_FD) ? session->ti.fd.in : session->ti.unixsock.sock;
         pfd.events = POLLIN;
         pfd.revents = 0;
         r = poll(&pfd, 1, 0);
@@ -1660,6 +1792,17 @@ nc_server_add_endpt(const char *name, NC_TRANSPORT_IMPL ti)
         }
         break;
 #endif
+    case NC_TI_UNIX:
+        server_opts.endpts[server_opts.endpt_count - 1].opts.unixsock = calloc(1, sizeof(struct nc_server_unix_opts));
+        if (!server_opts.endpts[server_opts.endpt_count - 1].opts.unixsock) {
+            ERRMEM;
+            ret = -1;
+            goto cleanup;
+        }
+        server_opts.endpts[server_opts.endpt_count - 1].opts.unixsock->mode = (mode_t)-1;
+        server_opts.endpts[server_opts.endpt_count - 1].opts.unixsock->uid = (uid_t)-1;
+        server_opts.endpts[server_opts.endpt_count - 1].opts.unixsock->gid = (gid_t)-1;
+        break;
     default:
         ERRINT;
         ret = -1;
@@ -1717,10 +1860,18 @@ nc_server_endpt_set_address_port(const char *endpt_name, const char *address, ui
         address = bind->address;
     }
 
+    if (!set_addr && endpt->ti == NC_TI_UNIX) {
+        ret = -1;
+        goto cleanup;
+    }
+
     /* we have all the information we need to create a listening socket */
-    if (address && port) {
+    if (address && (port || endpt->ti == NC_TI_UNIX)) {
         /* create new socket, close the old one */
-        sock = nc_sock_listen(address, port);
+        if (endpt->ti == NC_TI_UNIX)
+            sock = nc_sock_listen_unix(address, endpt->opts.unixsock);
+        else
+            sock = nc_sock_listen_inet(address, port);
         if (sock == -1) {
             ret = -1;
             goto cleanup;
@@ -1772,6 +1923,42 @@ nc_server_endpt_set_port(const char *endpt_name, uint16_t port)
 }
 
 API int
+nc_server_endpt_set_perms(const char *endpt_name, mode_t mode, uid_t uid, gid_t gid)
+{
+    struct nc_endpt *endpt;
+    uint16_t i;
+    int ret = 0;
+
+    if (!endpt_name) {
+        ERRARG("endpt_name");
+        return -1;
+    } else if (mode == 0) {
+        ERRARG("mode");
+        return -1;
+    }
+
+    /* ENDPT LOCK */
+    endpt = nc_server_endpt_lock_get(endpt_name, 0, &i);
+    if (!endpt)
+        return -1;
+
+    if (endpt->ti != NC_TI_UNIX) {
+        ret = -1;
+        goto cleanup;
+    }
+
+    endpt->opts.unixsock->mode = mode;
+    endpt->opts.unixsock->uid = uid;
+    endpt->opts.unixsock->gid = gid;
+
+cleanup:
+    /* ENDPT UNLOCK */
+    pthread_rwlock_unlock(&server_opts.endpt_lock);
+
+    return ret;
+}
+
+API int
 nc_server_del_endpt(const char *name, NC_TRANSPORT_IMPL ti)
 {
     uint32_t i;
@@ -1800,6 +1987,9 @@ nc_server_del_endpt(const char *name, NC_TRANSPORT_IMPL ti)
                 free(server_opts.endpts[i].opts.tls);
                 break;
 #endif
+            case NC_TI_UNIX:
+                free(server_opts.endpts[i].opts.unixsock);
+                break;
             default:
                 ERRINT;
                 /* won't get here ...*/
@@ -1841,6 +2031,9 @@ nc_server_del_endpt(const char *name, NC_TRANSPORT_IMPL ti)
                     free(server_opts.endpts[i].opts.tls);
                     break;
 #endif
+                case NC_TI_UNIX:
+                    free(server_opts.endpts[i].opts.unixsock);
+                    break;
                 default:
                     ERRINT;
                     break;
@@ -1969,7 +2162,14 @@ nc_accept(int timeout, struct nc_session **session)
         }
     } else
 #endif
-    {
+    if (server_opts.endpts[bind_idx].ti == NC_TI_UNIX) {
+        (*session)->data = server_opts.endpts[bind_idx].opts.unixsock;
+        ret = nc_accept_unix(*session, sock);
+        if (ret < 0) {
+            msgtype = NC_MSG_ERROR;
+            goto cleanup;
+        }
+    } else {
         ERRINT;
         close(sock);
         msgtype = NC_MSG_ERROR;

python/session.c
@@ -324,9 +324,14 @@ newChannel(PyObject *self)
 static PyObject *
 ncSessionStr(ncSessionObject *self)
 {
-    return PyUnicode_FromFormat("NETCONF Session %u to %s:%u (%lu references)", nc_session_get_id(self->session),
-                                nc_session_get_host(self->session), nc_session_get_port(self->session),
-                                ((PyObject*)(self))->ob_refcnt);
+    const char *host = nc_session_get_host(self->session);
+    if (host)
+        return PyUnicode_FromFormat("NETCONF Session %u to %s:%u (%lu references)", nc_session_get_id(self->session),
+                                    nc_session_get_host(self->session), nc_session_get_port(self->session),
+                                    ((PyObject*)(self))->ob_refcnt);
+    else
+        return PyUnicode_FromFormat("NETCONF Session %u to %s (%lu references)", nc_session_get_id(self->session),
+                                    nc_session_get_path(self->session), ((PyObject*)(self))->ob_refcnt);
 }
 
 /*
@@ -362,6 +367,12 @@ static PyObject *
 ncSessionGetHost(ncSessionObject *self, void *closure)
 {
     return PyUnicode_FromString(nc_session_get_host(self->session));
+}
+
+static PyObject *
+ncSessionGetPath(ncSessionObject *self, void *closure)
+{
+    return PyUnicode_FromString(nc_session_get_path(self->session));
 }
 
 static PyObject *

src/io.c
@@ -75,6 +75,7 @@ nc_read(struct nc_session *session, char *buf, size_t count, uint32_t inact_time
 {
     size_t readd = 0;
     ssize_t r = -1;
+    int fd;
     struct timespec ts_cur, ts_inact_timeout;
 
     assert(session);
@@ -96,22 +97,24 @@ nc_read(struct nc_session *session, char *buf, size_t count, uint32_t inact_time
             return 0;
 
         case NC_TI_FD:
+        case NC_TI_UNIX:
+            fd = (session->ti_type == NC_TI_FD) ? session->ti.fd.in : session->ti.unixsock.sock;
             /* read via standard file descriptor */
-            r = read(session->ti.fd.in, buf + readd, count - readd);
+            r = read(fd, buf + readd, count - readd);
             if (r < 0) {
                 if ((errno == EAGAIN) || (errno == EINTR)) {
                     r = 0;
                     break;
                 } else {
                     ERR("Session %u: reading from file descriptor (%d) failed (%s).",
-                        session->id, session->ti.fd.in, strerror(errno));
+                        session->id, fd, strerror(errno));
                     session->status = NC_STATUS_INVALID;
                     session->term_reason = NC_SESSION_TERM_OTHER;
                     return -1;
                 }
             } else if (r == 0) {
                 ERR("Session %u: communication file descriptor (%d) unexpectedly closed.",
-                    session->id, session->ti.fd.in);
+                    session->id, fd);
                 session->status = NC_STATUS_INVALID;
                 session->term_reason = NC_SESSION_TERM_DROPPED;
                 return -1;
@@ -552,9 +555,11 @@ nc_read_poll(struct nc_session *session, int io_timeout)
 #endif
         /* fallthrough */
     case NC_TI_FD:
-        if (session->ti_type == NC_TI_FD) {
+    case NC_TI_UNIX:
+        if (session->ti_type == NC_TI_FD)
             fds.fd = session->ti.fd.in;
-        }
+        else if (session->ti_type == NC_TI_UNIX)
+            fds.fd = session->ti.unixsock.sock;
 
         fds.events = POLLIN;
         fds.revents = 0;
@@ -649,6 +654,9 @@ nc_session_is_connected(struct nc_session *session)
     case NC_TI_FD:
         fds.fd = session->ti.fd.in;
         break;
+    case NC_TI_UNIX:
+        fds.fd = session->ti.unixsock.sock;
+        break;
 #ifdef NC_ENABLED_SSH
     case NC_TI_LIBSSH:
         return ssh_is_connected(session->ti.libssh.session);
@@ -692,7 +700,7 @@ struct wclb_arg {
 static int
 nc_write(struct nc_session *session, const void *buf, size_t count)
 {
-    int c;
+    int c, fd;
     size_t written = 0;
 #ifdef NC_ENABLED_TLS
     unsigned long e;
@@ -715,8 +723,12 @@ nc_write(struct nc_session *session, const void *buf, size_t count)
     do {
         switch (session->ti_type) {
         case NC_TI_FD:
-            c = write(session->ti.fd.out, (char *)(buf + written), count - written);
-            if (c < 0) {
+        case NC_TI_UNIX:
+            fd = session->ti_type == NC_TI_FD ? session->ti.fd.out : session->ti.unixsock.sock;
+            c = write(fd, (char *)(buf + written), count - written);
+            if (c < 0 && errno == EAGAIN) {
+                c = 0;
+            } else if (c < 0) {
                 ERR("Session %u: socket error (%s).", session->id, strerror(errno));
                 return -1;
             }

src/libnetconf.h
@@ -335,7 +335,9 @@
  *
  * To be able to accept any connections, endpoints must first be added
  * with nc_server_add_endpt() and configured with nc_server_endpt_set_address()
- * and nc_server_endpt_set_port().
+ * and nc_server_endpt_set_port(). For unix sockets, nc_server_endpt_set_perms()
+ * is available to set the unix socket file permissions, and nc_server_endpt_set_port()
+ * is invalid.
  *
  * Functions List
  * --------------
@@ -351,6 +353,7 @@
  * - nc_server_del_endpt()
  * - nc_server_endpt_set_address()
  * - nc_server_endpt_set_port()
+ * - nc_server_endpt_set_perms()
  *
  *
  * SSH

src/session.c
@@ -506,6 +506,19 @@ nc_session_get_host(const struct nc_session *session)
     return session->host;
 }
 
+API const char *
+nc_session_get_path(const struct nc_session *session)
+{
+    if (!session) {
+        ERRARG("session");
+        return NULL;
+    }
+    if (session->ti_type != NC_TI_UNIX)
+        return NULL;
+
+    return session->path;
+}
+
 API uint16_t
 nc_session_get_port(const struct nc_session *session)
 {
@@ -698,6 +711,12 @@ nc_session_free(struct nc_session *session, void (*data_free)(void *))
         (void)siter;
         break;
 
+    case NC_TI_UNIX:
+        sock = session->ti.unixsock.sock;
+        (void)connected;
+        (void)siter;
+        break;
+
 #ifdef NC_ENABLED_SSH
     case NC_TI_LIBSSH:
         if (connected) {
@@ -799,6 +818,7 @@ nc_session_free(struct nc_session *session, void (*data_free)(void *))
 
     lydict_remove(session->ctx, session->username);
     lydict_remove(session->ctx, session->host);
+    lydict_remove(session->ctx, session->path);
 
     /* final cleanup */
     if ((session->side == NC_SERVER) && session->opts.server.rpc_lock) {

src/session.h
@@ -65,6 +65,7 @@ typedef enum {
     NC_TI_NONE = 0,   /**< none - session is not connected yet */
     NC_TI_FD,         /**< file descriptors - use standard input/output, transport protocol is implemented
                            outside the current application */
+    NC_TI_UNIX,       /**< unix socket */
 #ifdef NC_ENABLED_SSH
     NC_TI_LIBSSH,     /**< libssh - use libssh library, only for NETCONF over SSH transport */
 #endif
@@ -176,6 +177,14 @@ const char *nc_session_get_host(const struct nc_session *session);
  * @return Session port.
  */
 uint16_t nc_session_get_port(const struct nc_session *session);
+
+/**
+ * @brief Get session path (unix socket only).
+ *
+ * @param[in] session Session to get the information from.
+ * @return Session unix socket path.
+ */
+const char *nc_session_get_path(const struct nc_session *session);
 
 /**
  * @brief Get session context.

src/session_client.c
@@ -22,12 +22,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <sys/un.h>
 #include <sys/stat.h>
 #include <sys/syscall.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <poll.h>
+#include <pwd.h>
 
 #include <libyang/libyang.h>
 
@@ -1115,6 +1117,101 @@ fail:
     return NULL;
 }
 
+API struct nc_session *
+nc_connect_unix(const char *address, struct ly_ctx *ctx)
+{
+    struct nc_session *session = NULL;
+    struct sockaddr_un sun;
+    const struct passwd *pw;
+    char *username;
+    int sock = -1;
+
+    if (address == NULL) {
+        ERRARG("address");
+        return NULL;
+    }
+
+    pw = getpwuid(geteuid());
+    if (pw == NULL) {
+        ERR("Failed to find username for euid=%u.\n", geteuid());
+        goto fail;
+    }
+
+    sock = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sock < 0) {
+        ERR("Failed to create socket (%s).", strerror(errno));
+        goto fail;
+    }
+
+    memset(&sun, 0, sizeof(sun));
+    sun.sun_family = AF_UNIX;
+    snprintf(sun.sun_path, sizeof(sun.sun_path), "%s", address);
+
+    if (connect(sock, (struct sockaddr *)&sun, sizeof(sun)) < 0) {
+        ERR("cannot connect to sock server %s (%s)",
+            address, strerror(errno));
+        goto fail;
+    }
+
+    if (fcntl(sock, F_SETFL, O_NONBLOCK) < 0) {
+        ERR("Fcntl failed (%s).", strerror(errno));
+        goto fail;
+    }
+
+    /* prepare session structure */
+    session = nc_new_session(NC_CLIENT, 0);
+    if (!session) {
+        ERRMEM;
+        goto fail;
+    }
+    session->status = NC_STATUS_STARTING;
+
+    /* transport specific data */
+    session->ti_type = NC_TI_UNIX;
+    session->ti.unixsock.sock = sock;
+    sock = -1; /* do not close sock in fail label anymore */
+
+    /* assign context (dictionary needed for handshake) */
+    if (!ctx) {
+        ctx = ly_ctx_new(NC_SCHEMAS_DIR, LY_CTX_NOYANGLIBRARY);
+        /* definitely should not happen, but be ready */
+        if (!ctx && !(ctx = ly_ctx_new(NULL, 0))) {
+            /* that's just it */
+            goto fail;
+        }
+    } else {
+        session->flags |= NC_SESSION_SHAREDCTX;
+    }
+    session->ctx = ctx;
+
+    session->path = lydict_insert(ctx, address, 0);
+
+    username = strdup(pw->pw_name);
+    if (username == NULL) {
+        ERRMEM;
+        goto fail;
+    }
+    session->username = lydict_insert_zc(ctx, username);
+
+    /* NETCONF handshake */
+    if (nc_handshake_io(session) != NC_MSG_HELLO) {
+        goto fail;
+    }
+    session->status = NC_STATUS_RUNNING;
+
+    if (nc_ctx_check_and_fill(session) == -1) {
+        goto fail;
+    }
+
+    return session;
+
+fail:
+    nc_session_free(session, NULL);
+    if (sock >= 0)
+        close(sock);
+    return NULL;
+}
+
 /*
    Helper for a non-blocking connect (which is required because of the locking
    concept for e.g. call home settings). For more details see nc_sock_connect().
@@ -1707,7 +1804,7 @@ nc_client_ch_add_bind_listen(const char *address, uint16_t port, NC_TRANSPORT_IM
         return -1;
     }
 
-    sock = nc_sock_listen(address, port);
+    sock = nc_sock_listen_inet(address, port);
     if (sock == -1) {
         return -1;
     }

src/session_client.h
@@ -145,6 +145,25 @@ void nc_client_destroy(void);
  */
 struct nc_session *nc_connect_inout(int fdin, int fdout, struct ly_ctx *ctx);
 
+/**
+ * @brief Connect to the NETCONF server via unix socket.
+ *
+ * Connect to netconf server via an unix socket. Function do not cover authentication
+ * or any other manipulation with the transport layer, it only establish NETCONF session
+ * by sending and processing NETCONF \<hello\> messages.
+ *
+ * @param[in] address Path to the unix socket.
+ * @param[in] ctx Optional parameter. If set, provides strict YANG context for the session
+ *                (ignoring what is actually supported by the server side). If not set,
+ *                YANG context is created for the session using \<get-schema\> (if supported
+ *                by the server side) or/and by searching for YANG schemas in the searchpath
+ *                (see nc_client_schema_searchpath()). In every case except not providing context
+ *                to connect to a server supporting \<get-schema\> it is possible that
+ *                the session context will not include all the models supported by the server.
+ * @return Created NETCONF session object or NULL in case of error.
+ */
+struct nc_session *nc_connect_unix(const char *address, struct ly_ctx *ctx);
+
 /**@} Client Session */
 
 #ifdef NC_ENABLED_SSH

commit 2f6221003d9e6bdc8860991ec498e4ae498f93f8

remove duplicated setsockopts for keepalive
When rebasing commit ac7fa2fb48a3 ("add support for unix socket
transport"), a conflict with commit be52dc29b169 ("session CHANGE
enable keep-alive for both server and client TCP sessions") was not
properly managed. This resulted in duplicated code in
nc_sock_accept_binds() to set the tcp keepalive socket options.

Remove the duplicated code from nc_sock_accept_binds(), since it is
already done in nc_sock_enable_keepalive().

Fixes: ac7fa2fb48a3 ("add support for unix socket transport")
Signed-off-by: Olivier Matz <olivier.matz@6wind.com>

src/session_server.c
@@ -389,34 +389,6 @@ nc_sock_accept_binds(struct nc_bind *binds, uint16_t bind_count, int timeout, ch
         return -1;
     }
 
-    if (saddr.ss_family == AF_INET || saddr.ss_family == AF_INET6) {
-        /* enable keep-alive */
-#ifdef TCP_KEEPIDLE
-        flags = 1;
-        if (setsockopt(ret, IPPROTO_TCP, TCP_KEEPIDLE, &flags, sizeof flags) == -1) {
-            ERR("Setsockopt failed (%s).", strerror(errno));
-            close(ret);
-            return -1;
-        }
-#endif
-#ifdef TCP_KEEPINTVL
-        flags = 5;
-        if (setsockopt(ret, IPPROTO_TCP, TCP_KEEPINTVL, &flags, sizeof flags) == -1) {
-            ERR("Setsockopt failed (%s).", strerror(errno));
-            close(ret);
-            return -1;
-        }
-#endif
-#ifdef TCP_KEEPCNT
-        flags = 10;
-        if (setsockopt(ret, IPPROTO_TCP, TCP_KEEPCNT, &flags, sizeof flags) == -1) {
-            ERR("Setsockopt failed (%s).", strerror(errno));
-            close(ret);
-            return -1;
-        }
-#endif
-    }
-
     if (idx) {
         *idx = i;
     }