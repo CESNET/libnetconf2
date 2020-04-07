bbf52c8b5260224e404a7f1787767015b9b69efc
Michal Vasko <mvasko@cesnet.cz>
2020-04-07 13:08:50 +0200

    client session CHANGE make linux-only header conditional

--- src/session_client.c
+++ src/session_client.c
@@ -13,6 +13,11 @@
  */
 
 #define _GNU_SOURCE
+
+#ifdef __linux__
+# include <sys/syscall.h>
+#endif
+
 #include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -25,7 +30,6 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <sys/stat.h>
-#include <sys/syscall.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <arpa/inet.h>
